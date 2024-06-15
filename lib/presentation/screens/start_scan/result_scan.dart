268b98ed4972c29d90a5a7234fb24bb7c4d69efb
ELshenawyy <alshnawyahmd668@gmail.com>
2024-06-15 17:52:39 +0300

    Result scan ui done

--- lib/presentation/screens/start_scan/result_scan.dart
+++ lib/presentation/screens/start_scan/result_scan.dart
@@ -2,160 +2,79 @@ import 'package:flutter/material.dart';
 import 'package:soil/app/app_color.dart';
 import 'package:soil/presentation/screens/start_scan/report.dart';
 
-class ResultScan extends StatelessWidget {
-  const ResultScan({super.key});
+import '../my_profile_view/help_view/custom_button.dart';
+
+class ResultScreen extends StatelessWidget {
+  final List<Map<String, String>> results = [
+    {"Element": "Nitrogen", "Value": "138 mg/kg"},
+    {"Element": "Phosphorous", "Value": "116 mg/kg"},
+    {"Element": "Potassium", "Value": "159 mg/kg"},
+  ];
+
+  ResultScreen({super.key});
+
+  Widget buildResultCard() {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: results.map((result) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(result["Element"] ?? ""),
+                Text(result["Value"] ?? ""),
+              ],
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Result Scan"),
+        title: const Text(
+          'Result Scan',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
         leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.arrow_back_ios_new_rounded),
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            // Add your back button action here
+          },
         ),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-         // height: MediaQuery.of(context).size.height,
-          child: Column(
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    SizedBox(height: 16.0),
+                    buildResultCard(),
+                    buildResultCard(),
+                    buildResultCard(),
+                    buildResultCard(),
+                    buildResultCard(),
 
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0xffF5F5F5),
-                    borderRadius: BorderRadius.circular(25),
-                    border: Border.all(color: AppColor.mainColor,)
-                  ),
-                  child: ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        return Container(
-                          height: 50,
-                          child: const Card(
-                            shape: Border(),
-                            child: ListTile(
-
-                              leading: Text(
-                                "element",
-                                style: TextStyle(
-                                    color: AppColor.mainColor, fontSize: 18),
-                              ),
-                              trailing: Text("value",
-                                  style: TextStyle(
-                                      color: AppColor.mainColor, fontSize: 18)),
-                            ),
-                          ),
-                        );
-                      },
-
-                      itemCount: 4),
+                  ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-
-                  decoration: BoxDecoration(
-                    color: const Color(0xffF5F5F5),
-                    borderRadius: BorderRadius.circular(25),
-                      border: Border.all(color: AppColor.mainColor,)
-                  ),
-                  child: ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        return Container(
-                          height: 50,
-                          child: const Card(
-                            child: ListTile(
-
-                              leading: Text(
-                                "element",
-                                style: TextStyle(
-                                    color: AppColor.mainColor, fontSize: 18),
-                              ),
-                              trailing: Text("value",
-                                  style: TextStyle(
-                                      color: AppColor.mainColor, fontSize: 18)),
-                            ),
-                          ),
-                        );
-                      },
-
-                      itemCount: 4),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-
-                  decoration: BoxDecoration(
-                    color: const Color(0xffF5F5F5),
-                    borderRadius: BorderRadius.circular(25),
-                      border: Border.all(color: AppColor.mainColor,)
-                  ),
-                  child: ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        return Container(height: 50,
-                          child: const Card(
-
-                            child: ListTile(
-                              horizontalTitleGap: 10,
-                              minVerticalPadding: 10,
-
-                              leading: Text(
-                                "element",
-                                style: TextStyle(
-                                    color: AppColor.mainColor, fontSize: 18),
-                              ),
-                              trailing: Text("value",
-                                  style: TextStyle(
-                                      color: AppColor.mainColor, fontSize: 18)),
-                            ),
-                          ),
-                        );
-                      },
-
-                      itemCount: 4),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Container(
-                  width: 327,
-                    height: 56,
-                    decoration: BoxDecoration(
-                      color: AppColor.mainColor,
-                      borderRadius: BorderRadius.circular(15)
-
-
-                    ),
-
-                    child: MaterialButton(onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (builder)=>const Report()));
-                    },child: Text("Stop Scan",style: TextStyle(color: Colors.white,fontSize: 18),),)),
-              )
-
-
-            ],
-
+            ),
           ),
-        ),
-
-
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: CustomButton(text: 'Go to report', onTap: () {  },),
+          ),
+        ],
       ),
-
-
-
-
-
     );
   }
 }
