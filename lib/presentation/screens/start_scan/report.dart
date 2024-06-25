a0f2d29a7f9e7ad642a8747ef238c906a8b07f4b
ELshenawyy <alshnawyahmd668@gmail.com>
2024-06-25 15:22:52 +0300

    NPK ai model done

--- lib/presentation/screens/start_scan/report.dart
+++ lib/presentation/screens/start_scan/report.dart
@@ -3,14 +3,18 @@ import 'package:flutter/material.dart';
 import '../../../app/app_color.dart';
 
 class Report extends StatelessWidget {
-  const Report({Key? key}) : super(key: key);
+  final List predictions;
+
+  Report({required this.predictions});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.pop(context);
+          },
           icon: const Icon(Icons.arrow_back_ios_new_rounded),
         ),
         title: const Text(
@@ -19,144 +23,86 @@ class Report extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-
-      body: const Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Center(child: CircularProgressIndicator(
-              color: AppColor.mainColor,
-            )),
-            // Padding(
-            //   padding: const EdgeInsets.all(10.0),
-            //   child: Row(
-            //     mainAxisAlignment: MainAxisAlignment.end,
-            //     children: [
-            //       TextButton(
-            //         onPressed: () {},
-            //         child: const Text(
-            //           "Download",
-            //           style: TextStyle(color: AppColor.mainColor, fontSize: 20),
-            //         ),
-            //       ),
-            //       const Icon(Icons.file_download_outlined,
-            //         color: AppColor.mainColor,size: 33,)
-            //
-            //
-            //
-            //     ],
-            //   ),
-            // ),
-            // Center(
-            //   child: DataTable(
-            //     columnSpacing: 70,
-            //     border: const TableBorder(),
-            //     decoration: BoxDecoration(
-            //         color:AppColor.mainColor,
-            //         border: Border.all(
-            //           color: AppColor.mainColor,
-            //         )),
-            //     columns: const[
-            //       DataColumn(
-            //           label: Text(
-            //         'Crops',
-            //         style: TextStyle(color: Colors.white),
-            //       )),
-            //       DataColumn(
-            //         label: Text('N', style: TextStyle(color: Colors.white)),
-            //       ),
-            //       DataColumn(
-            //           label: Text('P', style: TextStyle(color: Colors.white))),
-            //       DataColumn(
-            //           label: Text('K', style: TextStyle(color: Colors.white))),
-            //     ],
-            //     rows: const [
-            //       DataRow(
-            //           color: MaterialStatePropertyAll(Color(0xffFFFFFF)),
-            //           cells: <DataCell>[
-            //             DataCell(Text('Potatos')),
-            //             DataCell(Text('2.6')),
-            //             DataCell(Text('1')),
-            //             DataCell(Text('4.5')),
-            //           ]),
-            //       DataRow(
-            //           color: MaterialStatePropertyAll(Color(0xffDCCEC0)),
-            //           cells: <DataCell>[
-            //             DataCell(Text('Tomatoes')),
-            //             DataCell(Text('2.6')),
-            //             DataCell(Text('1')),
-            //             DataCell(Text('4.5')),
-            //           ]),
-            //       DataRow(
-            //           color: MaterialStatePropertyAll(Color(0xffFFFFFF)),
-            //           cells: <DataCell>[
-            //             DataCell(Text('Tomatoes')),
-            //             DataCell(Text('2.6')),
-            //             DataCell(Text('1')),
-            //             DataCell(Text('4.5')),
-            //           ]),
-            //       DataRow(
-            //           color: MaterialStatePropertyAll(Color(0xffDCCEC0)),
-            //           cells: <DataCell>[
-            //             DataCell(Text('Tomatoes')),
-            //             DataCell(Text('2.6')),
-            //             DataCell(Text('1')),
-            //             DataCell(Text('4.5')),
-            //           ]),
-            //       DataRow(
-            //           color: MaterialStatePropertyAll(Color(0xffFFFFFF)),
-            //           cells: <DataCell>[
-            //             DataCell(Text('Tomatoes')),
-            //             DataCell(Text('2.6')),
-            //             DataCell(Text('1')),
-            //             DataCell(Text('4.5')),
-            //           ]),
-            //       DataRow(
-            //           color: MaterialStatePropertyAll(Color(0xffDCCEC0)),
-            //           cells: <DataCell>[
-            //             DataCell(Text('Tomatoes')),
-            //             DataCell(Text('2.6')),
-            //             DataCell(Text('1')),
-            //             DataCell(Text('4.5')),
-            //           ]),
-            //       DataRow(
-            //           color: MaterialStatePropertyAll(Color(0xffFFFFFF)),
-            //           cells: <DataCell>[
-            //             DataCell(Text('Tomatoes')),
-            //             DataCell(Text('2.6')),
-            //             DataCell(Text('1')),
-            //             DataCell(Text('4.5')),
-            //           ]),
-            //       DataRow(
-            //           color: MaterialStatePropertyAll(Color(0xffDCCEC0)),
-            //           cells: <DataCell>[
-            //             DataCell(Text('Tomatoes')),
-            //             DataCell(Text('2.6')),
-            //             DataCell(Text('1')),
-            //             DataCell(Text('4.5')),
-            //           ]),
-            //
-            //       // ... more rows
-            //     ],
-            //   ),
-            // ),
-            // SizedBox(height: 25),
-            // Padding(
-            //   padding: const EdgeInsets.all(10.0),
-            //   child: Container(
-            //       width: 327,
-            //       height: 56,
-            //       decoration: BoxDecoration(
-            //           color: AppColor.mainColor,
-            //           borderRadius: BorderRadius.circular(15)),
-            //       child: MaterialButton(
-            //         onPressed: () {},
-            //         child: const Text(
-            //           "Select crop",
-            //           style: TextStyle(color: Colors.white, fontSize: 18),
-            //         ),
-            //       )),
-            // )
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Download",
+                      style: TextStyle(color: AppColor.mainColor, fontSize: 16),
+                    ),
+                  ),
+                  const Icon(
+                    Icons.file_download_outlined,
+                    color: AppColor.mainColor,
+                    size: 25,
+                  )
+                ],
+              ),
+            ),
+            Center(
+              child: DataTable(
+                columnSpacing: 70,
+                border: const TableBorder(),
+                decoration: BoxDecoration(
+                    color: AppColor.mainColor,
+                    border: Border.all(
+                      color: AppColor.mainColor,
+                    )),
+                columns: const [
+                  DataColumn(
+                      label: Text(
+                    'Crops',
+                    style: TextStyle(color: Colors.white),
+                  )),
+                  DataColumn(
+                    label: Text('N', style: TextStyle(color: Colors.white)),
+                  ),
+                  DataColumn(
+                      label: Text('P', style: TextStyle(color: Colors.white))),
+                  DataColumn(
+                      label: Text('K', style: TextStyle(color: Colors.white))),
+                ],
+                rows: predictions.map<DataRow>((prediction) {
+                  return DataRow(
+                    color: WidgetStateProperty.all(
+                        predictions.indexOf(prediction) % 2 == 0
+                            ? const Color(0xffFFFFFF)
+                            : const Color(0xffDCCEC0)),
+                    cells: <DataCell>[
+                      DataCell(Text(prediction['crop'])),
+                      DataCell(Text(prediction['NPK'][0].toString())),
+                      DataCell(Text(prediction['NPK'][1].toString())),
+                      DataCell(Text(prediction['NPK'][2].toString())),
+                    ],
+                  );
+                }).toList(),
+              ),
+            ),
+            const SizedBox(height: 25),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                width: 327,
+                height: 56,
+                decoration: BoxDecoration(
+                    color: AppColor.mainColor,
+                    borderRadius: BorderRadius.circular(15)),
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Select crop",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
